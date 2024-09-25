89f4bb53be376392e626d4df8496275864fa2a29
mithuraj24 <mithurajboruah@gmail.com>
2024-09-25 19:42:11 +0530

    Add files via upload

---
+++ classworkMrb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+float power(float,int);
+int main()
+{
+	float a,pow;
+	int b;
+	printf("\n Enter two numbers:");
+	scanf("%f %d",&a,&b);
+	pow=power(a,b);
+	printf("%f to the power %d=%f \n",a,b,pow);
+	return 0;
+}
+ float power(float a,int b)
+ {
+ 	int i;
+ 	float p=1;
+ 	for(i=1;i<=b;i++)
+ 	 p=p*a;
+ 	 return(p);
+ }
