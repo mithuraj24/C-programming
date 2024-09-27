ba22101b8c3ff91f5a9bda53229404dbb7c25c3c
mithuraj24 <mithurajboruah@gmail.com>
2024-09-27 18:12:39 +0530

    Add files via upload

---
+++ Stringinout.c
@@ -0,0 +1,13 @@
+// Program for string input output operation
+//Mithuraj Boruah
+//Date:27/09/24
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char fname[20];
+	printf("\nEnter first name:");
+	scanf("%s",fname);
+	printf("\nFirst Name:%s",fname);
+}
