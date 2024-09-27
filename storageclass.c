cc659e07c8169ef268a8bbcd667103e1aac86d1a
mithuraj24 <mithurajboruah@gmail.com>
2024-09-27 18:21:13 +0530

    Add files via upload

---
+++ storageclass.c
@@ -0,0 +1,16 @@
+// Program for storage class(static)
+//Mithuraj Boruah
+//Date27/09/24
+
+#include<stdio.h>
+main()
+{
+	sample();
+	sample();
+}
+sample()
+{
+	int x=20;
+	x+=10;
+	printf("%d",x);
+}
