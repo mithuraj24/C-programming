f3d7e98e07673925b8b86c3e4a13f19d0287c945
mithuraj24 <mithurajboruah@gmail.com>
2024-10-23 21:57:00 +0530

    Add files via upload

---
+++ October23-2024/Passreference(single var).c
@@ -0,0 +1,34 @@
+// Define and Initialize a structure
+//Mithuraj Boruah
+//22/10/24
+//Pass a reference to a function using pointer(single variable)
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+	char name[25];
+	int age;
+	char grade[5];
+};
+display(struct student *x);
+
+main()
+{
+	struct student s;
+	printf("Enter student name:");
+	scanf("%s",s.name);
+	printf("Enter student age:");
+	scanf("%d",&s.age);
+	printf("Enter student grade:");
+	scanf("%s",s.grade);
+	display(&s);
+	
+}
+
+display(struct student *x)
+{
+	printf("\nStudent Name: %s", x->name);
+	printf("\nStudent Age: %d", x->age);
+	printf("\nStudent Grade: %s", x->grade);
+	return(0);
+}
